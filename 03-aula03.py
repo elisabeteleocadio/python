64512273fb7f300527deb68e858fcebe7e326c64
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-08-29 11:50:56 -0300

    Add files via upload
    29.08.2024
    Elisabete Leocadio - 2°C
    Prof: Eduardo Hernandes

---
+++ 03-aula03.py
@@ -0,0 +1,16 @@
+# Variáveis em Python 
+# Não há necessidade de declará-lás no início do programa
+# Basta Criar no meio do algoritmo com a atribuição 
+
+teste = "Elisabete"
+print(teste) # Imprime a string "Eduardo"
+print(type(teste)) # Imprime  o tipo d\ variável, string
+teste = 10
+print(teste) # Imprime o número 10
+print(type(teste)) # Imprime o tipo da variável que é int
+teste = 1.74
+print(teste) # Imprime o número 1.74
+print(type(teste)) # Imprime o tipo de variável, que é float
+teste = True 
+print(teste) # Imprime o valor True
+print(type(teste)) # Imprime o tipo de variável, que é booll9
