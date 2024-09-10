4e51b8f43548ab100f5b1c545e3aa9d7c2dd7fa6
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-09-10 11:11:42 -0300

    Add files via upload
    Aula do dia 10.09.2024
    Professor: Eduardo Hernandes 
    Aluna: Elisabete Leocadio
    2°C

---
+++ 06-Operadores-aritmericos.py
@@ -0,0 +1,22 @@
+# Operadores aritméticos
+# Soma
+soma = 10 + 5
+print(soma)
+# Subtração
+Subtracao = 10 - 5
+print(subtracao)
+# Multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+# Divisão
+Divisao = 10/5
+print(divisao)
+# Divisão inteira
+Divisao_inteiro = 10 // 5
+print(divisao_inteira)
+# Exponenciação
+exponenciacao = 10 ** 5
+print (exponenciacao)
+# Módulo / Resto da Divisão
+modulo = 10 % 5
+print(modulo)
