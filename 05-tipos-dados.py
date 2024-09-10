1bee7e79f8209f1df859e51ba30a40158d1a3589
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-09-10 10:48:54 -0300

    Add files via upload
    Aula do dia 10.09.2024
    Prof: Eduardo 
    Aluna: Elisabete Rodrigues

---
+++ 05-tipos-dados.py
@@ -0,0 +1,13 @@
+# Tipos de dados em python
+# Inteiros 
+inteiro = 10
+print(inteiro)
+# Floats
+float = 10.5
+print(float)
+# Strings
+strings = "Olá, Mundo!"
+print(strings)
+# Booleano
+booleano = True 
+print(booleano)
