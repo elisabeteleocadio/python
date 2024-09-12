fb93f95b1b15f2ac74c0d804cefe1f3784ddb3c2
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-09-12 11:44:54 -0300

    Add files via upload
    Aula do dia 12.09.2024
    Prof: Eduardo Hernandes 
    .
    Objetivo: Criar um algoritmo que armazene dos valores distintos ( A= 5 e B=10).Depois efetue a troca de valores de forma que a variável A passe a possuir o valor da variável B e vice-versa. Por fim apresentar os valores trocados (A=10 e B=5).

---
+++ 08-trocarvalor.py
@@ -0,0 +1,15 @@
+# Declaração das variáveis 
+a = 5
+b = 10
+
+# Mostrar no prompt de comando
+print(f"Valores: A = {a}, B = {b}")
+print("Processando troca de valores...")
+
+#troca de valores
+# a,b = b,a (uma forma)
+temporario = a
+a = b
+b = temporario
+
+print(f"Valores agora: A = {a}, B = {b}" ) 
