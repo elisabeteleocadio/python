368213dfb2d565c89fd01c9c4d509d36b885ed4a
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-09-12 11:50:57 -0300

    Add files via upload
    Aula do dia 12.09.2024 
    Professor: Eduardo Hernandes 
    Aluna: Elisabete R. Leocadio 
    .
    09-par-ou-impar= Crie um algoritmo que receba um número inteiro e identifique se ele é par ou ímpar.

---
+++ 09-par-ou-impar.py
@@ -0,0 +1,10 @@
+# Começo já com a declaração das variávies
+# E com o input
+
+nota1 = int(input("Escreva um número: "))
+
+if (nota1 % 2 == 0):
+    print("[Seu número é PAR]")
+
+else:
+    print("[Seu núemro é ÍMPAR]")
