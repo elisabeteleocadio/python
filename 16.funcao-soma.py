82c9c427d7f377167c1d190e57ad46b11b574978
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-09-26 11:58:38 -0300

    Add files via upload
    Aula do dia 26.09.2024

---
+++ 16.funcao-soma.py
@@ -0,0 +1,11 @@
+# declaração de função somar
+def somar(a,b):
+    return a+b
+
+# algoritmo da soma
+print("algoritmo de soma")
+valor1 = int(input("digite o valor de 1: "))
+valor2 = int(input("digite o valor de 2: "))
+resultados = somar(valor1,valor2)
+
+print(f"o resultado da soma = {resultados}")
