a0aa25bd4effbcea2c0fe84858ed26a02f2d30c9
Elisabete Leocadio <elisabeteleocadio2008@gmail.com>
2024-10-17 11:54:50 -0300

    Delete 23-lacofor_numero.py

--- 23-lacofor_numero.py
+++
@@ -1,4 +0,0 @@
-#Um algoritmo que exibe a tabuada de um número fornecido pelo usuário.
-numero = int(input("Escolha Uma Tabuada: "))
-for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
